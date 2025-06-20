35f195c734223dc4695fc9e063fdc4a578760a3e
Sri Harsha <sriharshayedira06@gmail.com>
2025-06-20 17:23:02 +0530

    Time: 0 ms (100%), Space: 9.4 MB (22.25%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void solve(int i,int n,vector<vector<int>>& pos,vector<string>& board,           vector<vector<string>>& ans) {
+        if(i==n) {
+            ans.push_back(board);
+            return;
+        };
+
+        for(int j=0; j<n; j++) {
+            if(pos[0][j]==1 || pos[1][i+j]==1 || pos[2][i-j+n-1]==1) continue;
+            board[i][j] = 'Q';
+            pos[0][j] = 1;
+            pos[1][i+j] = 1;
+            pos[2][i-j+n-1] = 1;
+            solve(i+1,n,pos,board,ans);
+            board[i][j] = '.';
+            pos[0][j] = 0;
+            pos[1][i+j] = 0;
+            pos[2][i-j+n-1] = 0;
+        }
+    }
+
+    int totalNQueens(int n) {
+            vector<vector<string>> ans;
+            vector<string> board(n,string(n,'.'));
+            vector<int> col(n,0),d1(2*n-1,0),d2(2*n-1,0);
+            vector<vector<int>> pos;
+            pos.push_back(col);
+            pos.push_back(d1);
+            pos.push_back(d2);
+            solve(0,n,pos,board,ans);
+            return ans.size();
+    }
+};
