fd055dc9d5dfe546966e3aa7675ce7aef6a7e467
Sri Harsha <sriharshayedira06@gmail.com>
2025-06-22 14:54:38 +0530

    Time: 4 ms (37.03%), Space: 16.4 MB (18.45%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-    void generate(int n,string s,vector<string>& ans) {
+    void generate(int n,int cnt1,int cnt2,string s,vector<string>& ans) {
         if(s.size()==2*n) {
-            int cnt=0;
-            for(int i=0; i<2*n; i++) {
-                if(s[i]=='(') cnt++;
-                else cnt--;
-                if(cnt<0) return;
-            }
-            if(cnt==0) ans.push_back(s);
+            ans.push_back(s);
             return;
         }
 
-        s+='(';
-        generate(n,s,ans);
-        s.pop_back();
-        s+=')';
-        generate(n,s,ans);
-        s.pop_back();
+        cnt1++;
+        if(cnt1<=n) {
+            s+='(';
+            generate(n,cnt1,cnt2,s,ans);
+            s.pop_back();
+        }
+        cnt1--;
+        cnt2++;
+        if(cnt2<=cnt1) {
+            s+=')';
+            generate(n,cnt1,cnt2,s,ans);
+            s.pop_back();
+        }
+         cnt2--;
     }
 
     vector<string> generateParenthesis(int n) {
         string temp="";
+        int cnt1 = 0,cnt2 = 0;
         vector<string> ans;
-        generate(n,temp,ans);
+        generate(n,cnt1,cnt2,temp,ans);
         return ans;
     }
 };
