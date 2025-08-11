110c9b8b6db74539d8a4a96da7bb934c7f31c4af
Sri Harsha <sriharshayedira06@gmail.com>
2025-08-11 12:34:39 +0530

    Time: 20 ms (61.7%), Space: 147.6 MB (69.68%) - LeetHub

--- 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.cpp
+++ 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.cpp
@@ -3,15 +3,14 @@ public:
     vector<int> productQueries(int n, vector<vector<int>>& queries) {
         vector<int> ans,powers;
         for(int i=0; i<=29; i++) {
-            int x = 1<<i;
-            if((n & x) == x) powers.push_back(x);
+            if((n & 1<<i) != 0) powers.push_back(1<<i);
         }
         const int MOD = 1000000007;
         sort(powers.begin(),powers.end());
         for(int i=0; i<queries.size(); i++) {
-            int product = 1;
+            long long product = 1;
             for(int j=queries[i][0]; j<=queries[i][1]; j++) {
-                product = (1LL*(product % MOD)*(powers[j] % MOD)) % MOD;
+                product = (product*powers[j]) % MOD;
             }
             ans.push_back(product);
         }
