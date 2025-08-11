bd529232e23496fa69dd8e36fbb85ab35ea3452a
Sri Harsha <sriharshayedira06@gmail.com>
2025-08-11 12:29:56 +0530

    Time: 25 ms (50.53%), Space: 147.7 MB (58.51%) - LeetHub

--- 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.cpp
+++ 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     vector<int> productQueries(int n, vector<vector<int>>& queries) {
-        vector<int> powers,ans;
-        int sum = 0;
-        const int MOD = 1000000007;
-        for(int i=29; i>=0; i--) {
+        vector<int> ans,powers;
+        for(int i=0; i<=29; i++) {
             int x = 1<<i;
-            if(sum+x>n) continue;
-            sum+=x;
-            powers.push_back(x);
-            if(sum==n) break;
+            if((n & x) == x) powers.push_back(x);
         }
+        const int MOD = 1000000007;
         sort(powers.begin(),powers.end());
         for(int i=0; i<queries.size(); i++) {
             int product = 1;
