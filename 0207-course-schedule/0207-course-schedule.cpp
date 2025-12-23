ff66857b18c731e60e49fda5dba032c0d5c3c6a8
Sri Harsha <sriharshayedira06@gmail.com>
2025-12-24 00:47:56 +0530

    Time: 0 ms (100%), Space: 19 MB (92.09%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -3,32 +3,27 @@ public:
     void topoSort(vector<vector<int>>& adj,int& courses,vector<int>& inDeg) {
         queue<int> q;
         for(int i=0; i<inDeg.size(); i++) {
-            if(inDeg[i]==0) {
-                q.push(i);
-                courses++;
-            }
+            if(inDeg[i]==0) q.push(i);
         }
         while(!q.empty()) {
             int top = q.front();
             q.pop();
+            courses++;
             for(int &v : adj[top]) {
                 inDeg[v]--;
-                if(inDeg[v]==0) {
-                    q.push(v);
-                    courses++;
-                }
+                if(inDeg[v]==0) q.push(v);
             }
         }
     }
 
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
-        for(int i=0; i<prerequisites.size(); i++) {
-            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        for(auto &p : prerequisites) {
+            adj[p[1]].push_back(p[0]);
         }
         int courses = 0;
         vector<int> inDeg(numCourses);
-        for(auto neighbr : adj) {
+        for(auto &neighbr : adj) {
             for(int &v : neighbr) {
                 inDeg[v]++;
             }
