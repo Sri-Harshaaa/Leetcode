f0f87f198de3d80d63d0259930cf87ea3d805ae6
Sri Harsha <sriharshayedira06@gmail.com>
2025-12-26 13:55:02 +0530

    Time: 7 ms (67.76%), Space: 13.4 MB (81.2%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     int bestClosingTime(string customers) {
-        int n = customers.size();
-        vector<int> N(n+1,0), Y(n+1,0);
-        for(int i=1; i<n+1; i++) {
-            if(customers[i-1] == 'N') N[i] = N[i-1]+1;
-            else N[i] = N[i-1];
-        }
-        for(int j=n-1; j>=0; j--) {
-            if(customers[j] == 'Y') Y[j] = Y[j+1]+1;
-            else Y[j] = Y[j+1];
-        }
-        int penalty = INT_MAX, ans = -1;
-        for(int k=0; k<n+1; k++) {
-            if(Y[k]+N[k] < penalty) {
-                penalty = Y[k]+N[k];
-                ans = k;
+        int n = customers.size(),N = 0,Y = 0, ans = 0, temp = 0;
+        for(int i=1; i<=n; i++) {
+            if(customers[i-1] == 'Y') Y++;
+            else N++;
+            if(Y - N > temp) {
+                temp = Y-N;
+                ans = i;
             }
         }
         return ans;
