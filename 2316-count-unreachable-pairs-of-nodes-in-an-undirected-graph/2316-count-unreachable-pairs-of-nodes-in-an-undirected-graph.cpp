907b96d682fbc83d9c49946b4217edf6452374ca
Sri Harsha <sriharshayedira06@gmail.com>
2025-12-29 13:45:06 +0530

    Time: 44 ms (77.27%), Space: 150.8 MB (89.13%) - LeetHub

---
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    void Union(int x,int y,vector<int>& parent,vector<int>& rank) {
+        int leader_x = find(x,parent);
+        int leader_y = find(y,parent);
+        if(leader_x == leader_y) return;
+
+        if(rank[leader_x] < rank[leader_y]) parent[leader_x] = leader_y;
+        else if(rank[leader_x] > rank[leader_y]) parent[leader_y] = leader_x;
+        else {
+            parent[leader_x] = leader_y;
+            rank[leader_y]++;
+        }
+        return;
+    }
+
+    int find(int i,vector<int>& parent) {
+        if(parent[i] == i) return i;
+
+        return parent[i] = find(parent[i],parent);
+    }
+
+    long long countPairs(int n, vector<vector<int>>& edges) {
+        vector<int> parent(n), rank(n,0);
+        for(int i=0; i<n; i++) parent[i] = i;
+
+        for(int i=0; i<edges.size(); i++) {
+            Union(edges[i][0],edges[i][1],parent,rank);
+        }
+
+        unordered_map<int,int> mpp;
+        int comp = 0;
+        for(int i=0; i<n; i++) {
+            find(i,parent);
+            mpp[parent[i]]++;
+            if(parent[i]==i) comp++;
+        }
+        if(comp == 1) return 0;
+        long long pre_sum = 0, ans = 0;
+        for(auto &p : mpp) {
+            ans+=p.second*pre_sum;
+            pre_sum+=p.second;
+        }
+        return ans;
+    }
+};
