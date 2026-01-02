947cfe57f0c8e7191b00d80d449696ca2922824c
Sri Harsha <sriharshayedira06@gmail.com>
2026-01-02 11:33:08 +0530

    Time: 0 ms (100%), Space: 28.4 MB (87.61%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -5,7 +5,7 @@ public:
         int n = nums.size()/2,ans = -1;
         for(int i=0; i<2*n; i++) {
             mpp[nums[i]]++;
-            if(mpp[nums[i]] == n) {
+            if(mpp[nums[i]] == 2) {
                 ans = nums[i];
                 break;
             }
