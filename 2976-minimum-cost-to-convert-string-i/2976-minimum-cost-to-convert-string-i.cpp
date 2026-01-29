1019f06a75bf2a555e8fc8a8c0921fe90ea38ff1
Sri Harsha <sriharshayedira06@gmail.com>
2026-01-29 23:39:45 +0530

    Time: 30 ms (71.59%), Space: 100.2 MB (57.74%) - LeetHub

--- 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
@@ -1,34 +1,38 @@
 class Solution {
 public:
     long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
-        vector<vector<long long>> dist(26, vector<long long>(26, INT_MAX));
-        for(int i=0; i<original.size(); i++) {
-            int u = original[i]-'a';
-            int v = changed[i]-'a';
-            long long w = cost[i];
-            if(w < dist[u][v]) dist[u][v] = w;
+        const long long INF = 1e15; 
+        vector<vector<long long>> dist(26, vector<long long>(26, INF));
+
+        for(int i = 0; i < 26; i++) dist[i][i] = 0;
+
+        for(int i = 0; i < original.size(); i++) {
+            int u = original[i] - 'a';
+            int v = changed[i] - 'a';
+            dist[u][v] = min(dist[u][v], (long long)cost[i]);
         }
 
-        for(int k=0; k<26; k++) {
-            for(int i=0; i<26; i++) {
-                for(int j=0; j<26; j++) {
-                    if(i==j) {
-                        dist[i][j] = 0;
-                        continue;
+        for(int k = 0; k < 26; k++) {
+            for(int i = 0; i < 26; i++) {
+                for(int j = 0; j < 26; j++) {
+                    if (dist[i][k] != INF && dist[k][j] != INF) {
+                        dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
                     }
-                    if(dist[i][k]+dist[k][j] < dist[i][j]) dist[i][j] = dist[i][k]+dist[k][j];
                 }
             }
         }
 
-        long long min_cost = 0;
-        for(int i=0; i<source.size(); i++) {
-            int u = source[i]-'a';
-            int v = target[i]-'a';
-            long long c = dist[u][v];
-            if(c == INT_MAX) return -1;
-            else min_cost+=c;
+        long long total_min_cost = 0;
+        for(int i = 0; i < source.size(); i++) {
+            if (source[i] == target[i]) continue;
+            
+            int u = source[i] - 'a';
+            int v = target[i] - 'a';
+            
+            if (dist[u][v] >= INF) return -1;
+            total_min_cost += dist[u][v];
         }
-        return min_cost;
+
+        return total_min_cost;
     }
 };
