c04d9d2425b42c7664a7b566393bb0c089042f1b
Sri Harsha <sriharshayedira06@gmail.com>
2026-05-13 20:17:35 +0530

    Time: 2169 ms (12.05%), Space: 476.3 MB (10.84%) - LeetHub

--- 3493-properties-graph/3493-properties-graph.cpp
+++ 3493-properties-graph/3493-properties-graph.cpp
@@ -1,23 +1,5 @@
 class Solution {
 public:
-    int find(int i, vector<int>& parent) {
-        if(parent[i] == i) return i;
-
-        return parent[i] = find(parent[i],parent);
-    }
-
-    void Union(int x, int y, vector<int>& parent, vector<int>& rank) {
-        int x_leader = find(x,parent);
-        int y_leader = find(y,parent);
-
-        if(rank[y_leader] < rank[x_leader]) parent[y_leader] = x_leader;
-        else if(rank[y_leader] > rank[x_leader]) parent[x_leader] = y_leader;
-        else {
-            parent[x_leader] = y_leader;
-            rank[y_leader]++;
-        }
-    }
-
     int intersect(vector<int>& a, vector<int>& b) {
         unordered_set<int> setA(a.begin(), a.end());
         int count = 0;
@@ -27,23 +9,36 @@ public:
         return count;
     }
 
+    void dfs(int u, vector<vector<int>>& adj, vector<bool>& visited) {
+        visited[u] = true;
+
+        for(int &v: adj[u]) {
+            if(!visited[v]) dfs(v,adj,visited);
+        }
+    }
+
     int numberOfComponents(vector<vector<int>>& properties, int k) {
         int n = properties.size();
-        int components = n;
+        int components = 0;
         vector<int> rank(n,0), parent(n);
         for(int i=0; i<n; i++) {
             parent[i]=i;
         }
+        vector<vector<int>> adj(n);
         for(int i=0; i<n; i++) {
             for(int j=i+1; j<n; j++) {
                 if(intersect(properties[i],properties[j]) >= k) {
-                    int i_leader = find(i,parent);
-                    int j_leader = find(j,parent);
-                    if(i_leader != j_leader) {
-                        Union(i,j,parent,rank);
-                        components--;
-                    }
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
                 }
+            }
+        }
+
+        vector<bool> visited(n,false);
+        for(int i=0; i<n; i++) {
+            if(!visited[i]) {
+                dfs(i,adj,visited);
+                components++;
             }
         }
         return components;
