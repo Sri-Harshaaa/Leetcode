486f13be7c30e63a681dfa1c75f16af131816c70
Sri Harsha <sriharshayedira06@gmail.com>
2026-06-01 21:15:00 +0530

    Time: 299 ms (51.59%), Space: 197.3 MB (45.84%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    vector<int> maybe1, maybe2;
-
-    int solve(int u, int parent, vector<vector<int>>& adj, string& s) {
+    int solve(int u, int parent, vector<vector<int>>& adj, string& s, int& ans) {
         int m1 = 0, m2 = 0;
         for(int& v : adj[u]) {
             if(v == parent) continue;
-            int sol = solve(v,u,adj,s);
-            if(s[u] == s[v]) {
-                maybe1.push_back(sol);
-                continue;
-            }
+            int sol = solve(v,u,adj,s,ans);
+            if(s[u] == s[v]) continue;
             if(sol >= m1) {
                 m2 = m1;
                 m1 = sol;
             }
             else if(sol > m2) m2 = sol;
         }
-        maybe2.push_back(m1+m2+1);
+        ans = max(ans,m1+m2+1);
+
         return m1+1;
     }
 
@@ -28,11 +24,9 @@ public:
             adj[i].push_back(parent[i]);
             adj[parent[i]].push_back(i);
         }
-        int ans = solve(0,-1,adj,s);
-        int maxi1 = -1, maxi2 = -1;
-        if(maybe1.size() > 0) maxi1 = *max_element(maybe1.begin(), maybe1.end());
-        if(maybe2.size() > 0) maxi2 = *max_element(maybe2.begin(), maybe2.end());
-
-        return max({ans,maxi1,maxi2});
+        int ans = 0;
+        solve(0,-1,adj,s,ans);
+        
+        return ans;
     }
 };
