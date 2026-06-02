214628b1801ded075865814458a162fef979cd3b
Sri Harsha <sriharshayedira06@gmail.com>
2026-06-02 16:57:56 +0530

    Time: 63 ms (54.22%), Space: 132.8 MB (66.17%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,58 +1,33 @@
 class Solution {
 public:
-    int find(int i, vector<int>& parent) {
-        if(i == parent[i]) return i;
+    void dfs(int u, int& ans, vector<bool>& visited, vector<vector<pair<int,int>>>& adj) {
+        visited[u] = true;
 
-        return parent[i] = find(parent[i],parent);
-    }
+        for(auto& neighbr : adj[u]) {
+            int v = neighbr.first;
+            int w = neighbr.second;
 
-    void Union(int x, int y, vector<int>& parent, vector<int>& rank) {
-        int x_leader = find(x,parent);
-        int y_leader = find(y,parent);
+            ans = min(ans,w);
 
-        if(x_leader == y_leader) return;
-
-        if(rank[x_leader] < rank[y_leader]) parent[x_leader] = y_leader;
-        else if(rank[y_leader] < rank[x_leader]) parent[y_leader] = x_leader;
-        else {
-            parent[x_leader] = y_leader;
-            rank[y_leader]++;
+            if(!visited[v]) dfs(v,ans,visited,adj);
         }
     }
 
     int minScore(int n, vector<vector<int>>& roads) {
-        vector<int> parent(n+1), rank(n+1,0);
-        for(int i=0; i<n+1; i++) {
-            parent[i] = i;
-        }
-
-        for(int i=0; i<roads.size(); i++) {
-            int u = roads[i][0];
-            int v = roads[i][1];
-
-            Union(u,v,parent,rank);
-        }
-
-        for(int i=1; i<n+1; i++) {
-            find(i,parent);
-        }
-
-        int leader = parent[1];
-
-        int ans = INT_MAX;
-
+        vector<vector<pair<int,int>>> adj(n+1);
         for(int i=0; i<roads.size(); i++) {
             int u = roads[i][0];
             int v = roads[i][1];
             int w = roads[i][2];
 
-            int u_leader = find(u,parent);
-            int v_leader = find(v,parent);
-
-            if(u_leader != leader || v_leader != leader) continue;
-
-            ans = min(ans,w);
+            adj[u].push_back({v,w});
+            adj[v].push_back({u,w});
         }
+        
+        int ans = INT_MAX;
+        vector<bool> visited(n+1,false);
+
+        dfs(1,ans,visited,adj);
 
         return ans;
     }
