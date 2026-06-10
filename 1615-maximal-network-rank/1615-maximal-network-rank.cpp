f60a31e91bb5cbbd2c4773050c712e056aaa3803
Sri Harsha <sriharshayedira06@gmail.com>
2026-06-10 13:35:51 +0530

    Time: 72 ms (8.62%), Space: 43.8 MB (25.03%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -2,14 +2,19 @@ class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
         int ans = INT_MIN;
+        vector<set<int>> adj(n);
+        for(int i=0; i<roads.size(); i++) {
+            int u = roads[i][0];
+            int v = roads[i][1];
+
+            adj[u].insert(v);
+            adj[v].insert(u);
+        }
 
         for(int i=0; i<n; i++) {
             for(int j=i+1; j<n; j++) {
-                int cnt = 0;
-                for(int k=0; k<roads.size(); k++) {
-                    if(roads[k][0] == i || roads[k][1] == i) cnt++;
-                    else if(roads[k][0] == j || roads[k][1] == j) cnt++;
-                }
+                int cnt = adj[i].size() + adj[j].size();
+                if(adj[i].count(j)) cnt--;
                 ans = max(ans,cnt);
             }
         }
