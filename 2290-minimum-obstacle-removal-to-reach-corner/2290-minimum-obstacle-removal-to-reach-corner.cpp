be92180acdb8fdbe7b9b830456f31871a65ae7bf
Sri Harsha <sriharshayedira06@gmail.com>
2026-06-17 14:39:42 +0530

    Time: 108 ms (83.27%), Space: 121.7 MB (48.71%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
@@ -9,21 +9,18 @@ public:
         int n = grid[0].size();
         vector<int> dx = {0,0,1,-1};
         vector<int> dy = {1,-1,0,0};
+        deque<pair<int,int>> dq;
         vector<vector<int>> dist(m, vector<int>(n,INT_MAX));
-        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
 
         dist[0][0] = grid[0][0] == 0 ? 0 : 1;
-        pq.push({dist[0][0],{0,0}});
+        dq.push_front({0,0});
 
-        while(!pq.empty()) {
-            auto p = pq.top();
-            pq.pop();
-            int d = p.first;
-            int x = p.second.first;
-            int y = p.second.second;
-
+        while(!dq.empty()) {
+            pair<int,int> p = dq.front();
+            dq.pop_front();
+            int x = p.first;
+            int y = p.second;
             if(x == m-1 && y == n-1) return dist[x][y];
-            if(d > dist[x][y]) continue;
 
             for(int i=0; i<4; i++) {
                 int u = x + dx[i];
@@ -31,9 +28,15 @@ public:
 
                 if(isValid(u,v,m,n)) {
                     int w = grid[u][v];
-                    if(d+w < dist[u][v]) {
-                        dist[u][v] = d+w;
-                        pq.push({d+w,{u,v}});
+                    if(dist[x][y]+w >= dist[u][v]) continue;
+
+                    if(w==0) {
+                        dist[u][v] = dist[x][y];
+                        dq.push_front({u,v});
+                    }
+                    else {
+                        dist[u][v] = dist[x][y]+1;
+                        dq.push_back({u,v});
                     }
                 } 
             }
