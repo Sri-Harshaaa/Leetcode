c70277b14d978ec2e9aab3a3aaeb1c6b1aecfbaf
Sri Harsha <sriharshayedira06@gmail.com>
2026-07-06 08:36:13 +0530

    Time: 1 ms (42.14%), Space: 16.4 MB (16.55%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -7,34 +7,26 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<int> dx = {1,0};
-        vector<int> dy = {0,1};
+        vector<int> dx = {-1,0};
+        vector<int> dy = {0,-1};
         vector<vector<int>> dist(m, vector<int>(n,INT_MAX));
         dist[0][0] = grid[0][0];
-        priority_queue<tuple<int,int,int>, vector<tuple<int,int,int>>, greater<tuple<int,int,int>>> pq;
-        pq.push({dist[0][0],0,0});
 
-        while(!pq.empty()) {
-            auto t = pq.top();
-            pq.pop();
-            int d = get<0>(t);
-            int x = get<1>(t);
-            int y = get<2>(t);
+        for(int x=0; x<m; x++) {
+            for(int y=0; y<n; y++) {
+                if(x==0 && y==0) continue;
 
-            if(d > dist[x][y]) continue;
-            if(x == m-1 && y == n-1) return dist[m-1][n-1];
+                for(int i=0; i<2; i++) {
+                    int u = x + dx[i];
+                    int v = y + dy[i];
 
-            for(int i=0; i<2; i++) {
-                int u = x + dx[i];
-                int v = y + dy[i];
-
-                if(isValid(u,v,m,n) && (d+grid[u][v] < dist[u][v])) {
-                    dist[u][v] = d+grid[u][v];
-                    pq.push({dist[u][v],u,v});
-                } 
+                    if(isValid(u,v,m,n) && dist[u][v] != INT_MAX) {
+                        dist[x][y] = min(dist[x][y], dist[u][v]+grid[x][y]);
+                    }
+                }
             }
         }
 
-        return -1;
+        return dist[m-1][n-1];
     }
 };
